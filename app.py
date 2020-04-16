1b0916af3153065e299fe42a2049056b120a3773
katsukawagami <joelai1988@gmail.com>
2020-04-16 20:49:42 +0800

    [新增]templates資料夾,render_template功能

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return "Hello, Flask"
+    title = 'man Key'
+    return render_template('index.html', title=title)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app.run()
