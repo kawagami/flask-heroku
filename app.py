de1b6f1cc65268509a6df6e2bd5ca1c4676faeff
katsukawagami <joelai1988@gmail.com>
2020-04-17 12:34:47 +0800

    [新增]if for 繼承 引用

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     title = 'man Key'
-    return render_template('index.html', title=title)
+    data = [
+        '張三',
+        '李四',
+        '王大拿'
+    ]
+    return render_template('index.html', title=title, data=data)
 
 
 if __name__ == "__main__":
-    app.run()
+    # debug=True的話之後在run本檔案時修改本檔案會自動重新rerun一般
+    app.run(debug=True)
